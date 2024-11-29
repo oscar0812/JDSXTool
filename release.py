f789434e5d08d475487eefee0dbf761f54eee33e
Oscar <oscar0812torres@gmail.com>
2024-11-28 22:30:30 -0600

    Release version 1.0.9

--- release.py
+++ release.py
@@ -16,7 +16,20 @@ content = re.sub(r"version\s*=\s*'.*'", f"version = '{new_version}'", content)
 with open("build.gradle", "w") as file:
     file.write(content)
 
-print(f"All instances of version updated to {new_version}.")
+print(f"Version in build.gradle updated to {new_version}.")
+
+try:
+    with open("gradle.properties", "r") as file:
+        properties_content = file.read()
+
+    properties_content = re.sub(r"^version\s*=\s*'.*'$", f"version={new_version}", properties_content, flags=re.M)
+
+    with open("gradle.properties", "w") as file:
+        file.write(properties_content)
+
+    print(f"Version in gradle.properties updated to {new_version}.")
+except FileNotFoundError:
+    print("gradle.properties file not found. Skipping update for gradle.properties.")
 
 try:
     subprocess.run(["git", "add", "."], check=True)
@@ -26,8 +39,8 @@ try:
 
     subprocess.run(["git", "tag", new_version], check=True)
 
-    subprocess.run(["git", "push"], check=True)  # Push changes (commits)
-    subprocess.run(["git", "push", "--tags"], check=True)  # Push tags
+    subprocess.run(["git", "push"], check=True)
+    subprocess.run(["git", "push", "--tags"], check=True)
 
     print(f"Successfully committed and tagged version {new_version}.")
 
